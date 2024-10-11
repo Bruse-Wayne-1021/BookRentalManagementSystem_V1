25eb98c86d58d6d12459bbe62c4c5754125ba825
Sajilan P. <UT01261@unicomtic.com>
2024-10-11 14:19:42 +0530

    Question three

--- BookRentalManagementSystem_V1/Book.cs
+++ BookRentalManagementSystem_V1/Book.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +11,77 @@ namespace BookRentalManagementSystem_V1
 {
     internal class Book
     {
-        private int bookIdid {  get; set; }
+        private int bookId {  get; set; }
         private string title { get; set; }
         private string author { get; set; }
         private decimal rentalPrice { get; set; }
 
+
+        public string Gettitle(string title)
+        {
+            return title;
+        }
+
+        public int GetBookid(int bookId)
+        {
+            return bookId;
+        }
+
+
+        public string Getauthor(string author)
+        {
+            return author;
+        }
+        public  decimal GetRentalPrice(decimal rentalPrice)
+        {
+            return rentalPrice;
+        }
+
+
+        
+        public void SetTitle(string Title)
+        {
+            title = Title;
+        }
+
+        public void SetBookid(int Bookid )
+        {
+            bookId = Bookid;
+        }
+
+        public void SetAuthor(string Author)
+        {
+            author = Author;
+        }
+
+        public void SetRentalPrice(decimal RentalPrice)
+        {
+            rentalPrice = RentalPrice;
+        }
+
+
+
+
+
+        public Book(int Bookid,string Title,string Author,decimal RentalPrice)
+        {
+            //bookId= Bookid;
+            //title= Title;
+            //author= Author;
+            //rentalPrice = RentalPrice;
+        }
+        
+
+        public override string ToString()
+        {
+            return $"Book id :{bookId}, Book title :{title} , Book Author :{author} , Rental price:{rentalPrice}";
+        }
+        
+
+        public virtual string DisplayBooksInfo()
+        {
+            return $"Book id :{bookId}, Book title :{title} , Book Author :{author} , Rental price:{rentalPrice}";
+        }
 
         
         
